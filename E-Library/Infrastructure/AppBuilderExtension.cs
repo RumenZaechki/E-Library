db916498e244593669c33c3c5e72336d5aab20c5
RumenZaechki <rumenzaechki@protonmail.com>
2022-03-08 10:44:24 +0200

    Added an adminSeeder function

--- E-Library/Infrastructure/AppBuilderExtension.cs
+++ E-Library/Infrastructure/AppBuilderExtension.cs
@@ -1,5 +1,6 @@
 ﻿using E_Library.Data;
 using E_Library.Data.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace E_Library.Infrastructure
@@ -9,10 +10,39 @@ namespace E_Library.Infrastructure
         public static IApplicationBuilder PrepareDatabase(this IApplicationBuilder app)
         {
             using var scopedServices = app.ApplicationServices.CreateScope();
-            var data = scopedServices.ServiceProvider.GetService<LibraryDbContext>();
+            var data = scopedServices.ServiceProvider.GetRequiredService<LibraryDbContext>();
+            var serviceProvider = scopedServices.ServiceProvider;
             data.Database.Migrate();
             SeedCategories(data);
+            SeedAdministratior(serviceProvider);
             return app;
+        }
+        private static void SeedAdministratior(IServiceProvider services)
+        {
+            var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+            Task
+                .Run(async () =>
+                {
+                    if (await roleManager.RoleExistsAsync("Administrator"))
+                    {
+                        return;
+                    }
+                    await roleManager.CreateAsync(new IdentityRole
+                    {
+                        Name = "Administrator"
+                    });
+                    const string adminPassword = "admin123";
+                    var user = new IdentityUser
+                    {
+                        Email = "admin@mail.com",
+                        UserName = "admin@mail.com"
+                    };
+                    await userManager.CreateAsync(user, adminPassword);
+                    await userManager.AddToRoleAsync(user, "Administrator");
+                })
+                .GetAwaiter()
+                .GetResult();
         }
         private static void SeedCategories(LibraryDbContext data)
         {
