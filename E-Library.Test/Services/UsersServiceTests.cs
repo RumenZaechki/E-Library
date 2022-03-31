a4a581c69443569c5ac5e231857990f0565211bc
RumenZaechki <rumenzaechki@protonmail.com>
2022-03-31 11:12:36 +0300

    Added UsersServiceTests

---
+++ E-Library.Test/Services/UsersServiceTests.cs
@@ -0,0 +1,76 @@
+﻿using E_Library.Data.Models;
+using E_Library.Services.Users;
+using E_Library.Test.Mocks;
+using System.Linq;
+using Xunit;
+
+namespace E_Library.Test.Services
+{
+    public class UsersServiceTests
+    {
+        [Fact]
+        public void GetAllUsersShouldReturnCorrectResultWhenThereAreUsersInTheDb()
+        {
+            var data = DbMock.Instance;
+            var user = GetUser();
+            data.Users.Add(user);
+            data.SaveChanges();
+            var usersService = new UsersService(data);
+
+            var result = usersService.GetAllUsers("").ToList();
+
+            Assert.NotEmpty(result);
+            Assert.Equal(result[0].Username, user.UserName);
+        }
+
+        [Fact]
+        public void GetAllUsersShouldReturnEmptyCollectionWhenNoUsersAreInTheDb()
+        {
+            var data = DbMock.Instance;
+            var usersService = new UsersService(data);
+
+            var result = usersService.GetAllUsers("");
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void DeleteUserWorksCorrectlyWhenGivenCorrectInput()
+        {
+            var data = DbMock.Instance;
+            var user = GetUser();
+            data.Users.Add(user);
+            data.SaveChanges();
+
+            Assert.Single(data.Users);
+
+            var usersService = new UsersService(data);
+            usersService.DeleteUser(user.Id);
+
+            Assert.Empty(data.Users);
+        }
+
+        [Fact]
+        public void DeleteUserDoesNothingWhenGivenIncorrectInput()
+        {
+            var data = DbMock.Instance;
+            var user = GetUser();
+            data.Users.Add(user);
+            data.SaveChanges();
+            var usersService = new UsersService(data);
+
+            usersService.DeleteUser("invalidUserId");
+
+            Assert.NotEmpty(data.Users);
+        }
+
+        private User GetUser()
+        {
+            return new User
+            {
+                UserName = "RandomDude",
+                Cart = new Cart()
+            };
+        }
+    }
+}
